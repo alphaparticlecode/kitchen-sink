801a232a1bbc97816c4f9627f25972b8f4b2dd72
Keanan Koppenhaver <keanan@alphaparticle.com>
2022-08-12 14:22:05 -0500

    Sets up plugin information

---
+++ kitchen-sink.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       Kitchen Sink
+ * Plugin URI:        https://wordpress.org/plugins/kitchen-sink/
+ * Description:       Generates a "kitchen sink" page with examples of Gutenberg block implementations.
+ * Version:           1.0
+ * Requires at least: 6.0
+ * Requires PHP:      7.2
+ * Author:            Alpha Particle
+ * Author URI:        https://alphaparticle.com/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       kitchen-sink
+ */
